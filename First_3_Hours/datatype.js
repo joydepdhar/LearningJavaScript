1024268ca296dc8cfe1703ec2684be0728bba765
joydepdhar <joydepdhar138@gmai.com>
2025-03-23 23:57:07 +0600

    1.56hr done

--- First_3_Hours/datatype.js
+++ First_3_Hours/datatype.js
@@ -1,15 +1,50 @@
-let name= "Joydep"
-let age=22
-let is_logged_in=false
-//number ==> 2 to power 53
-//string ==> ""
-//bigint
-//boolean => true or false
-//null => standalone value==> object
-//undefined=> undefined
-//symbol=> unique in react
+// let name= "Joydep"
+// let age=22
+// let is_logged_in=false
+// //number ==> 2 to power 53
+// //string ==> ""
+// //bigint
+// //boolean => true or false
+// //null => standalone value==> object
+// //undefined=> undefined
+// //symbol=> unique in react
 
-//object
-console.log(typeof(name));
-console.log(typeof null)
-console.log(typeof undefined)
+// //object
+// console.log(typeof(name));
+// console.log(typeof null)
+// console.log(typeof undefined)
+//Primitve (call by value)
+/*
+7 type: string , Number , Boolean ,null, undefined , symbol, BigInt,
+
+ */
+const score =100
+const score_value =100.33
+const is_logged_in=false
+const outside_temp=null;//empty not zero
+let user_email// value undefined
+
+const id =Symbol("123")
+const another_id =Symbol("123")
+const big_number=2132132113213231231314413n //bigInt declare
+
+
+// console.log(id=== another_id);
+
+
+
+//Non Primitive (call by reference)
+/*
+array, objects, Functions
+ */
+const heros =['Naagraj','Doga','Shaktiman']
+let my_object={
+    name:'Joydep',
+    age:22,
+}
+
+const myfunction= function(){
+    console.log("Hello! Joydep");
+    
+}
+console.log(typeof(myfunction))
---
+++ First_3_Hours/operation.js
@@ -0,0 +1,46 @@
+let value=2
+let negValue=-value
+// console.log(negValue)
+let str1="Hello! "
+let str2="Joydep"
+let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2")
+// console.log("1"+2+3)
+// console.log(1+2+"2")
+// console.log(+true)
+// console.log(+"")
+
+
+let num1, num2, num3;
+num1=num2=num3=2+2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// let account_number=1000;
+// ++account_number;
+// console.log(account_number);
+// account_number++
+// console.log(account_number)
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2">1)
+// console.log("02">1)
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log()
+// console.log(undefined==0)
+// console.log(undefined===0)
+// console.log(undefined>=0)
+// console.log(undefined>0)
+
+//=== it check datatype also
+console.log("2"===2);
+
