ae689eefeaf2fa12601e0054e9088ae5abddf3a6
joydepdhar <joydepdhar138@gmai.com>
2025-03-26 12:54:50 +0600

    today update 4.51hr

---
+++ First_3_Hours/function.js
@@ -0,0 +1,18 @@
+function say_my_name(){
+    console.log("J");
+    console.log("o");
+    console.log("y");
+    console.log("d");
+    console.log("e");
+    console.log("p"); 
+}
+
+say_my_name()
+function add_numbers_problem(num1, num2){
+    console.log(num1+num2);
+}
+console.log(add_numbers_problem(10,20))
+function add_Numbers(num1,num2){
+    return num1+num2;
+}
+// console.log(add_Numbers(10,15))
---
+++ First_3_Hours/more_object_more.js
@@ -0,0 +1,12 @@
+const course={
+    course_name:"Js",
+    price:"999",
+    course_instructor:"Joydep Dhar"
+}
+const {course_instructor:instructor}=course
+// console.log(instructor)
+
+// const navbar=({company})=>{
+
+// }
+// navbar(company="Joydep")
