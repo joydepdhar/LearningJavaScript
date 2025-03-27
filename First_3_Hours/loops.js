c6e91e154386f7a7af16df9f19609dffaad47b08
joydepdhar <joydepdhar138@gmai.com>
2025-03-27 17:13:14 +0600

    9hr complete js revision

---
+++ First_3_Hours/loops.js
@@ -0,0 +1,33 @@
+// // // basic loop for loop
+
+// // // for (let index = 0; index <=5; index++) {
+// // //     const element = index;
+// // //     console.log(index);
+        
+// // // }
+// // for(let i=0;i<=5;i++){
+// //     console.log(`outer loop value: ${i}`);
+// //     for(let j=0;j<=5;j++){
+// //         console.log(`inner loop value ${j}`);
+        
+// //     }
+// // }
+
+let my_array=["ironman","superman","hulk","flash"]
+// for (let index = 0; index < my_array.length; index++) {
+//     const element = my_array[index];
+//     console.log(element);
+    
+    
+// }
+// let index=0;
+// while(index<my_array.length){
+//     console.log(my_array[index]);
+//     index=index+1;
+// }
+// let index=0;
+// do{
+//     console.log(my_array[index]);
+//     index++;
+// }
+// while(index<my_array.length)
