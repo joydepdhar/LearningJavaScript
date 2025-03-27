a68c75ef9033fee8783a453dfe13cf4a4a5cbbdb
joydepdhar <joydepdhar138@gmai.com>
2025-03-27 13:37:13 +0600

    6.55Hr completed

---
+++ First_3_Hours/controlflow.js
@@ -0,0 +1,64 @@
+// if(true){
+//     //always excute
+// }
+// if(false){
+//     // never excute
+// }
+
+// const is_user_logged_in=true;
+
+// if(is_user_logged_in){
+//     console.log("Okey and user logged in");
+// }
+// const temp =29
+
+// if(temp<30){
+//     console.log("Less than 30");
+// }else{
+//     console.log("temp is greater then 30")
+// }
+
+// const score =200
+
+// if(score>100){
+//     const power="fly";
+//     console.log(`user power: ${power}`);
+// }
+//not productive and don't use it
+// const balance =1000;
+// if (balance>500) console.log("test"),
+// console.log("test2");
+// const balance=1000;
+// if(balance<500){
+//     console.log("less then 500");
+// }else if(balance<800){
+//     console.log("less then 800");
+// }else if(balance<1000){
+//     console.log("less then 1000");
+// }else{
+//     console.log("less then 1200");
+// }
+const month=3;
+switch (month) {
+    case 1:
+        console.log("Jan");
+        
+        break;
+    case 2:
+        console.log("Feb");
+        
+        break;
+    case 3:
+        console.log("Mar");
+        
+        break;
+    case 4:
+        console.log("Apr");
+        
+        break;
+
+    default:
+        console.log("Default Match No match");
+        
+        break;
+}
